e5d6ca85f238c456e5a8c3d3d112bbec8e6f9cca
brulicharis <94843687+brulicharis@users.noreply.github.com>
2022-01-28 11:11:26 +0100

    Add files via upload

---
+++ kontakt.js
@@ -0,0 +1,13 @@
+const contact = [];
+function posalji(){
+    var student = {
+        name : document.getElementById("name").value,
+        mail : document.getElementById("mail").value,
+        message : document.getElementById("message").value,
+        
+        
+    };
+    alert("Uspješno ste poslali Vašu poruku. Hvala Vam!");
+    contact.push(contact);
+    console.log(contact);
+};
